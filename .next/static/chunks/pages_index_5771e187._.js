b468b05925d7b29c152c8c89f8c45896ebca0796
dharm2112 <gabanidharmce24@gmail.com>
2025-07-12 13:18:23 +0530

    Add .next/static/chunks/pages_index_5771e187._.js 1752306505838

---
+++ .next/static/chunks/pages_index_5771e187._.js
@@ -0,0 +1,13 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_next_85a7fad4._.js",
+  "static/chunks/node_modules_react-dom_82bb97c6._.js",
+  "static/chunks/node_modules_@firebase_firestore_dist_index_esm2017_06c08cb5.js",
+  "static/chunks/node_modules_@firebase_auth_dist_esm2017_a026d113._.js",
+  "static/chunks/node_modules_cf2405b0._.js",
+  "static/chunks/[root-of-the-server]__52ed5ab1._.js",
+  "static/chunks/styles_41d24a8f._.css"
+],
+    source: "entry"
+});
